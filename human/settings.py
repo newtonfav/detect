807ee88143fc5ee96bd52e9ec9b36c23807d8327
Favour Oghenekowho <gh.oghene@gmail.com>
2023-09-10 20:03:00 +0100

    fix settings

--- human/settings.py
+++ human/settings.py
@@ -35,8 +35,6 @@ SESSION_COOKIE_SECURE = True
 
 SECURE_SSL_REDIRECT = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
