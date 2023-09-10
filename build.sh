6be56486f76cc71415c99dc7c2eadce430e08328
Favour Oghenekowho <gh.oghene@gmail.com>
2023-09-10 21:57:46 +0100

    added poetry to project

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip && pip install -r requirements.txt
+poetry install
 
 python manage.py collectstatic --no-input
 python manage.py migrate
