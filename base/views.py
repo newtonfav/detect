e2186278090a747fa128c08807e3a1781977c769
Favour Oghenekowho <gh.oghene@gmail.com>
2023-09-11 10:48:34 +0100

    fix

--- base/views.py
+++ base/views.py
@@ -23,15 +23,16 @@ def upload_file(request):
             uploaded_file = request.FILES['file']
 
             file_name = uploaded_file.name  # Get the original file name
+            # directory_path = os.path.join(STATIC_FILES, 'uploads')
 
             form.save()
             face_img = detect_face(f'./../media/uploads/{file_name}')
             os.remove(os.path.join(settings.MEDIA_ROOT,
                                    f'./../media/uploads/{file_name}'))
+            file_names = os.listdir(settings.STATIC_FILES)
 
             # Redirect to a success page or do something else
-            # return redirect('success-page', arg1=image_url, arg2=face_img)
-            return render(request, 'success.html', {'response': face_img})
+            return render(request, 'success.html', {'response': face_img, 'image_url': file_names[0]})
     else:
         form = FileUploadForm()
     return render(request, './upload.html', {'form': form})
